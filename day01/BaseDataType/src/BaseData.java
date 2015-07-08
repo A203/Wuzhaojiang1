9e8adb180f71343cf4ee8013966d594ad65052c1
TMACCAMT <9912565671@qq.com>
2015-07-08 09:05:40 +0800

    Day01

---
+++ day01/BaseDataType/src/BaseData.java
@@ -0,0 +1,17 @@
+
+public class BaseData {
+	public static void main(String[] args){
+		System.out.println("基本类型:byte二进制位数"+Byte.SIZE);
+		System.out.println("基本类型:byte最大值"+Byte.MAX_VALUE);
+		System.out.println("基本类型:byte最小值"+Byte.MIN_VALUE);
+		byte b=50;
+		float f=10.0f;
+		char ch1="A";
+			
+				
+				
+				
+				
+	}
+
+}
